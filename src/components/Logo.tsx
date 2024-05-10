9410514deeed1da070aa9a59b10cdd2dfc8bad35
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-05-10 23:27:34 +0530

    wip

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,17 +1,19 @@
 // Logo Component that redirects users to the homepage.
 // It is used across various parts of the application to provide a consistent way to return to the main page.
 
+'use client';
+import { useTheme } from 'next-themes';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Logo() {
+  const { theme } = useTheme();
+  const logoSrc = theme === 'dark' ? '/dark-logo.png' : '/light-logo.png';
+
   return (
     <Link href='/'>
-      <div className='flex items-center gap-2'>
-        <Image src='/logo.png' className='size-8' width={50} height={50} alt='logo' />
-        <p className='text-lg not-italic font-extrabold leading-6 text-[#3C3C3C] dark:text-white'>
-          BuilderKit
-        </p>
+      <div className='flex items-center gap-2 w-fit'>
+        <Image src={logoSrc} width={150} height={128} alt='logo' />
       </div>
     </Link>
   );
