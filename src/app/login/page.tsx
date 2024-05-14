62ba11844e5bf7f1036d0970f11ded1b24183b89
hevinkalathiya <hevinvnsgu@gmail.com>
2024-05-14 21:20:15 +0530

    fix : linting

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,14 +10,14 @@ import { Separator } from '@/components/ui/separator';
 export default function Login() {
   return (
     <div className='h-screen flex'>
-      <div className='w-full flex flex-col items-center justify-center px-6'> <div className='text-2xl md:text-4xl font-medium mb-6 text-center space-y-2'>
+      <div className='w-full flex flex-col items-center justify-center px-6'>
+        <div className='text-2xl md:text-4xl font-medium mb-6 text-center space-y-2'>
           <p>AI Content Writer Tool </p>
           <p className='text-lg'>by Builderkit.ai</p>
         </div>
         <h2 className='text-[#3E3E3E] dark:text-[#98A5A8] text-sm mt-3 mb-9 font-medium'>
           Login or register with your email
         </h2>
-
         <div className='w-full sm:max-w-md flex flex-col gap-6 items-center'>
           <GoogleAuth />
           <Separator className='w-3/4 bg-[#A5ABB6]/20' />
